8794d6d562326378ea037c142706ac37f4123572
Fasjul <fasjul@gmail.com>
2016-03-06 16:38:35 +0100

    bugs shift arith

--- Elec_III/IEEE_mult.c
+++ Elec_III/IEEE_mult.c
@@ -1,19 +1,23 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "elec.h"
 
 uint32_t IEEE_mult(uint32_t measure1, uint32_t measure2){
-    int signe1 = measure1>>31;
-    int signe2 = measure2>>31;
-    int signe_glob;
+    uint32_t signe1 = measure1>>31;
+    uint32_t signe2 = measure2>>31;
+
+    uint32_t signe_glob;
     int normal [1];
     uint32_t exponent_correction;
     uint32_t result;
-    uint32_t norm = 1 << 22;
+    uint32_t norm = 1 << 23;
     uint32_t exposant, exposant1, exposant2;
     uint32_t norm_exp = (1 << 7) -1;
 
     signe_glob = (signe1 == signe2) ? 0 : 1; //bit de signe déterminé par xor
     signe_glob = signe_glob << 31;
+
+    printf("%d\n", signe_glob);
 
     uint32_t mantisse1 = (measure1<<9) >>9;
     mantisse1 += norm; //on ajoute le 1 devant les mantisses
@@ -22,7 +26,8 @@ uint32_t IEEE_mult(uint32_t measure1, uint32_t measure2){
     //uint32_t mantmult = shiftandadd(mantisse1, mantisse2, normal); //on a la nouvelle mantisse ainsi que la position du 1 de poids fort
 
 
-    uint32_t mantmult = (1<<20) + (1<<18) + (3<<16) + (1<<22);
+    uint32_t mantmult = (5<<28) + (1<<26) + (3<<23);
+    mantmult = mantmult >>7;
     normal[0] = 23;
 
 
@@ -31,13 +36,16 @@ uint32_t IEEE_mult(uint32_t measure1, uint32_t measure2){
 
     exposant1 = (measure1 << 1) >> 23;
     exposant2 = (measure2 << 1) >> 23;
-    exposant = IEEE_sub(exposant1, norm_exp);
+    /*exposant = IEEE_sub(exposant1, norm_exp);
     exposant = IEEE_add(exposant, exposant2);
-    exposant = IEEE_add(exposant, exponent_correction);
+    exposant = IEEE_add(exposant, exponent_correction);*/
+    exposant = exposant1 + exposant2 - norm_exp + exponent_correction;
     exposant = exposant << 22;
 
-    result = IEEE_add(result, exposant);
-    result = IEEE_add(result, signe_glob);
+    /*result = IEEE_add(result, exposant);
+    result = IEEE_add(result, signe_glob);*/
+
+    result += exposant +signe_glob;
 
     return result;
 
--- Elec_III/main.c
+++ Elec_III/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "elec.h"
 
-int main()
-{
-    uint32_t A = (3 << 30) + (3 << 23) + (1<20);
+
+int main(){
+
+    char *tmp;
+    char *bin = "10000000000000000000000000000000";
+    uint32_t res = (uint32_t) strtol(bin, &tmp, 2);
+
+    printf("%d\n", res);
+
+    uint32_t A = (3 << 30) + (3 << 23) + (1<<20);
     uint32_t B = (1<<30) + (1<<24) + (3<<19);
-    uint32_t C = ;
-    if (IEEE_mult(A, B) ==)
-    printf("%d", C);
+    uint32_t C = (3<<30) + (3<<24) + (5<<19) +(3<<16);
+    uint32_t D= IEEE_mult(A, B);
+    if (D == C){
+        printf("Hourra");
+
+    }
+    printf("%u, %u, %u\n", A, B, C);
     return 0;
 }
